3903a6961154fe5830fc270d4faf7bf9fba87c8d
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-04-14 17:36:19 -0400

    building data object for post request

--- server.js
+++ server.js
@@ -1,17 +1,62 @@
 const config = require('./config');
 const https = require('https');
+const request = require('request');
 
 // cannot use shippo SDK library
 
 class ProviderAbstract {
 
   constructor() {
-    const options = {
-      hostname: "api.goshippo.com",
-      path: "/shipments",
-      headers: {Authorization: `ShippoToken ${config.shippo.token}`}
+    this.options = {
+      url: "https://api.goshippo.com/shipments/",
+      headers: {
+        "Authorization": `ShippoToken ${config.shippo.token}`,
+        "Content-Type": "application/json"
+      },
+      data: {
+        "address_to": {
+          "name": "Romain Peynichou",
+          "street1": "3878 St Laurent Blvd",
+          "city": "Montreal",
+          "state": "QC",
+          "zip": "H2W1Y2",
+          "country": "CA",
+          "phone": "5146773006",
+          "email": "romainpeynichou@gmail.com"
+        },
+        "async": false,
+        "customs_declaration": {
+          "contents_type": "MERCHANDISE",
+          "contents_explanation": "Shipping ain't easy!",
+          "non_delivery_option": "RETURN",
+          "certify": true,
+          "certify_signer": "Shippo",
+          "items": [
+            {
+              "description": "My Parcel",
+              "quantity": 1,
+              "net_weight": "2",
+              "mass_unit": "lb",
+              "value_amount": 13.37,
+              "value_currency": "USD",
+              "metadata": "Your first Customs Item",
+              "origin_country": "US"
+            }
+          ],
+          "metadata": "Your first Customs Declaration!"
+        },
+        "parcels": [
+          {
+            "length": "5",
+            "width": "5",
+            "height": "5",
+            "distance_unit": "in",
+            "weight": "2",
+            "mass_unit": "lb"
+          }
+        ]
+      }
     }
-    return options;
   }
 
   getRates(addressObject) {
